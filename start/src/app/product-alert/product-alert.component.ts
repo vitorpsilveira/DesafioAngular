dcf5ae0699f4ff0a80f06500deab3c1502d4add7
Vitor Silveira <silveira_vitor@outlook.com>
2020-07-04 08:49:47 -0300

    t2

--- start/src/app/product-alert/product-alert.component.ts
+++ start/src/app/product-alert/product-alert.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-alert',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-alert.component.css']
 })
 export class ProductAlertComponent implements OnInit {
+
+  @Input() product;
 
   constructor() { }
 
